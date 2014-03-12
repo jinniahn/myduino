35290afd9d9a0b13e3d63b43c61caf756c731519
jinsub ahn <jinniahn@gmail.com>
2014-03-12 18:34:12 +0900

    init.

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+// load duino module
+var arduino = require('duino');
+var board = new arduino.Board({
+  debug: true
+});
+// create led object to control LED of D13
+var led = new arduino.Led({
+  board: board,
+  pin: 13
+});
+
+// GET /led_on
+app.get('/led_on', function(req, res){
+    led.on();
+});
+
+// GET /led_off
+app.get('/led_off', function(req, res){
+    led.off();
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
